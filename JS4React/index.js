8118ed6e6cc03e74c421b377848fd1438250a1d8
ElVittor <93359780+ElVittor@users.noreply.github.com>
2024-05-22 14:56:11 -0600

    Fin de práctica 2
    vimos funciones y DOM

--- JS4React/index.js
+++ JS4React/index.js
@@ -13,19 +13,42 @@ const suma = (a,b)=> {
 }
 
 //Fat Arrow Function Simpñificada
-const suma = (a,b)=> a+b */
+const suma = (a,b)=> a+b 
 
 //Objetos
-const usuario={
-    nombre:"Victor",
+const usuario = {
+    nombre: "Victor",
     apellidoP: "Lozano",
-    Edad:35,
-    direccion:{//OBjeto dentro del objeto
-        pais:"México",
-        ciudad:"Querétaro",
-        calle:'Mirador de las Cumbres'
+    Edad: 35,
+    direccion: { // Objeto dentro del objeto
+        pais: "México",
+        ciudad: "Querétaro",
+        calle: 'Mirador de las Cumbres'
     },
-    amigos:['Pedro','Picapiedra'],
-    estatus:true,
-    enviaCorreo: enviarCorr() => 'enviando ...'
+    amigos: ['Pedro', 'Picapiedra'],
+    estatus: true,
+    enviaCorreo: function() { 
+        return 'enviando ...';
+    }
 }
+
+console.log(usuario)
+console.log(usuario.direccion.calle)
+console.log(usuario.amigos)
+console.log(usuario.enviaCorreo())*/
+
+//Trabajo con DOM
+
+const titulo= document.createElement('h1');
+titulo.innerText='Título creado con JS';
+document.body.append(titulo);
+
+const boton = document.createElement('button');
+boton.innerText = 'Presiona aquí';
+document.body.append(boton);
+
+boton.addEventListener('click',function(){
+    //console.log('Evento Click Ejecutando ... ')
+    titulo.innerText="Evento Click Ejecutado";
+    alert('Se Logró');
+});
