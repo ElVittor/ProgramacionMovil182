1d059398570b03a85d483c1acf76cbd72f321dc4
ElVittor <93359780+ElVittor@users.noreply.github.com>
2024-06-17 16:41:18 -0600

    practica 8
    practica 8 botones e inputs

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,53 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React,{useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, TouchableHighlight, Pressable } from 'react-native';
+import React, {useState} from 'react'; /* para manejo de estados de componentes */
 
-const Texto =({estilo})=>{
+/* const Texto =({estilo})=>{
   const [contenido, setContenido] = useState('Hola Mundo :D');
   const cambiarTexto = () => {
     setContenido('Hola a todos');
   } 
   return (<Text style={[styles.text, estilo]} onPress={cambiarTexto}>{contenido}</Text>);
-} 
+}  */
 
 export default function App() {
-  const [contenido, setContenido]= useState('Hola Mundo')
-  const actualizarContenido=()=>{setContenido('State actualizó este texto')};
+  const[text, setText] = useState('Dale: ');/* crea un estado con un valor inicial */
+  const[submit, setSubmit] = useState('');
   return (
-    <View style={styles.container}>
-      
-      <Texto estilo={styles.red} />
-      <Texto estilo={styles.green} />
-      <Texto estilo={styles.blue} />
-
-      <StatusBar style="auto" />
-    </View>
+  <View style={styles.container}>
+    <Pressable style={({ pressed }) => [{backgroundColor: pressed ? '#0056b3' : '#007BFF'}, styles.button]} onPress={() => { setSubmit(text); alert('Texto Enviado'); }}>
+      <View> {/* Manda error de children con esto empaquetamos el hig en un solo view y evita ese error */}
+        <Text>Componente TextInput: {text} </Text>
+        <TextInput style={styles.input} placeholder='Soy un Input: ' onChangeText={(t) => setText(t)} value={text} /> {/* crea una linea de entrada de texto */}
+        <Button style={styles.buttonText} title='Presionaaaaaaaa!!!!!!' onPress={() => { setSubmit(text); alert('Texto Enviado'); }} />
+      </View>
+    </Pressable>
+    <StatusBar style="auto" />
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection:'column',
     backgroundColor: '#fff',
-    alignItems: 'center',/* izquieda a derecha */
-    justifyContent: 'center',/* trabaja de arriba hacia abajo */
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  text:{
-    color:'Yellow',
-    fontsiSize:25,
-    height:150,
-    width:150,
+ 
+  input:{
+    height:40,
+    width:'80%',
+    borderColor:'gray',
+    borderBottomWidth:1,
+    borderBottomColor:'black',
   },
-red:{
-  flex: 2,
-  backgroundColor:'red',
-},
-green:{
-  flex: 2,
-  backgroundColor:'green',
-},
-blue:{
-  flex: 2,
-  backgroundColor:'blue',
-},
+   button: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16, 
+  },
 });
