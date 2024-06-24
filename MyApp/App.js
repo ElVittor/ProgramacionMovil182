aaaaadc7e624e76c16c1fae095a61d30f5539d0a
ElVittor <93359780+ElVittor@users.noreply.github.com>
2024-06-24 15:28:54 -0600

    FlatList
    se aplica flat lista a la practica hola mundo

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,107 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, TouchableHighlight, Pressable, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
 import React, {useState} from 'react'; /* para manejo de estados de componentes */
-
-/* const Texto =({estilo})=>{
-  const [contenido, setContenido] = useState('Hola Mundo :D');
-  const cambiarTexto = () => {
-    setContenido('Hola a todos');
-  } 
-  return (<Text style={[styles.text, estilo]} onPress={cambiarTexto}>{contenido}</Text>);
-}  */
 
 export default function App() {
   const[text, setText] = useState('Dale: ');/* crea un estado con un valor inicial */
   const[submit, setSubmit] = useState('');
   return (
   <View style={styles.container}>
-    <ScrollView styles={styles.ScrollView} >
-
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-      <text>Componente: {submit} </text>
-
-    <Pressable style={({ pressed }) => [{backgroundColor: pressed ? '#0056b3' : '#007BFF'}, styles.button]} onPress={() => { setSubmit(text); alert('Texto Enviado'); }}>
-      <View> {}
-        <Text>Componente TextInput: {text} </Text>
-        <TextInput style={styles.input} placeholder='Soy un Input: ' onChangeText={(t) => setText(t)} value={text} /> {/* crea una linea de entrada de texto */}
-        <Button style={styles.buttonText} title='Presionaaaaaaaa!!!!!!' onPress={() => { setSubmit(text); alert('Texto Enviado'); }} />
-      </View>
-    </Pressable>
+    <FlatList data={[
+      {key:1, name: 'Nepomuceno Pámfilo'},
+      {key:2, name: 'SkyWalker Luke'},
+      {key:3, name: 'Potter Harry'},
+      {key:4, name: 'Wayne Bruce'},
+      {key:5, name: 'Lane Loise'},
+      {key:6, name: 'Flintstone Peter'},
+    ]}
+    renderItem={(Item)=><Text style={styles.item} > {Item.name} </Text> }
+    />
+    
     <StatusBar style="auto" />
-    </ScrollView>
+    
   </View>
   );
 }
@@ -114,25 +32,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
- 
-  input:{
-    height:40,
-    width:'80%',
-    borderColor:'gray',
-    borderBottomWidth:1,
-    borderBottomColor:'black',
-  },
-   button: {
-    backgroundColor: '#007BFF',
-    padding: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 16, 
-  },
+item:{
+  padding:10,
+  fontSize:24,
+  height:50,
+  borderColor: 'Pink',
+  borderBottomWidth:1,
+}
 
-  ScrollView:{
-    width: Dimensions.get('window').width /* para que se ajuste al ancho de la pantalla independientemente del dispositivo */
-  }
 });
